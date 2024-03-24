c1c192b70199fe0260d205a13f0e84fc0c30fb7a
DanielLaptop <danchiappori@gmail.com>
2024-03-24 13:15:40 +0100

    setting up genes structure

---
+++ Components/genetics.py
@@ -0,0 +1,19 @@
+"""
+GENETIC MATERIAL
+Provides possible actions for neural network brain to process.
+Actions are associated with various properties within the ecosystem.
+"""
+
+# IMPORTS
+pass
+
+
+# OBJECTS
+class GENE:
+
+    pass
+
+
+# RUN
+if __name__ == "__main__":
+    gene = GENE()
