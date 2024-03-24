4a77cba4053b85e921c7fa8f2b7efc27b0a001e8
DanielLaptop <danchiappori@gmail.com>
2024-03-24 08:26:46 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+"""
+ECOSYSTEM EVOLUTION SIMULATOR
+"""
+
+# IMPORTS
+pass
+
+
+# MAIN OBJECT
+class SIM:
+
+    def main(self):
+
+        print("TEMP")
+
+
+# RUN
+if __name__ == "__main__":
+    obj = SIM()
+    obj.main()
