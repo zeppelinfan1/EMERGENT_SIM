1a42ba7eb9fc9d6294c062cf062d5805bfb11817
Daniel <danchiappori@gmail.com>
2025-03-03 19:08:37 +0100

    mapping now a part of the subject's genetics. Hilbert curve mapping to change each gene into a pair of coordinate points between (-1 and 1) for each.

---
+++ Components/mapping.py
@@ -0,0 +1,27 @@
+# IMPORTS
+import numpy as np
+import hashlib
+from dataclasses import dataclass, fields
+from Components.subject import Subject
+
+
+"""
+Mapping each gene to a certain functionality.
+                        ^Energy Produced
+                        |
+                        |
+                        |
+                        |
+Destruction <--------------------------> Creation
+                        |
+                        |
+                        |
+                        |
+                         Energy Consumed
+"""
+
+class Mapping:
+
+    def __init__(self, ):
+
+        pass
--- Components/subject.py
+++ Components/subject.py
@@ -11,16 +11,39 @@ class Genetics:
     gene_number: int
     gene_length: int
     genes: list = field(init=False)
+    mapping: list = field(init=False)
 
     def __post_init__(self):
 
         # Binary array for each neuron
         self.genes = [self.generate_gene() for _ in range(self.gene_number)]
+        # Mapping for each gene
+        self.mapping = self.generate_map()
 
     def generate_gene(self):
 
         # Random binary list
         return [random.randint(0, 1) for _ in range(self.gene_length)]
+
+    def generate_map(self):
+
+        mapping_list = []
+        for gene in self.genes:
+
+            half = len(gene) // 2  # Split gene into two parts
+            x_binary = gene[:half]  # First half for X
+            y_binary = gene[half:]  # Second half for Y
+            # Mapping a binary gene to a Hilbert curve coordinate
+            x_decimal = int("".join(map(str, x_binary)), 2)
+            y_decimal = int("".join(map(str, y_binary)), 2)
+            max_value = (2 ** half) - 1  # Max value for scaling
+            x_value = (x_decimal / max_value) * 2 - 1
+            y_value = (y_decimal / max_value) * 2 - 1
+
+            mapping_list.append((x_value, y_value))
+
+        return mapping_list
+
 
     def __repr__(self):
 
@@ -45,8 +68,6 @@ class Subject:
 
 
 subject1 = Subject(gene_number=6, gene_length=10)
-print(subject1)
-
-
+print(subject1.genetics.mapping)
 
 
