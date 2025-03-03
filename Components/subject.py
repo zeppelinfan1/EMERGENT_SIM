cc14587007ef6accd709206dd4ef85e0630c403c
Daniel <danchiappori@gmail.com>
2025-03-03 10:45:18 +0100

    Creating subject and genetics dataclasses. Randomly initialized.

--- Components/subject.py
+++ Components/subject.py
@@ -1,24 +1,52 @@
-"""
-INDIVIDUAL SUBJECTS
-"""
-
 # IMPORTS
-from Components.nn_brain import Brain
+import numpy as np
+import random
+from dataclasses import dataclass, field
 
 
-# OBJECTS
+# DATACLASSES
+@dataclass
+class Genetics:
+
+    gene_number: int
+    gene_length: int
+    genes: list = field(init=False)
+
+    def __post_init__(self):
+
+        # Binary array for each neuron
+        self.genes = [self.generate_gene() for _ in range(self.gene_number)]
+
+    def generate_gene(self):
+
+        # Random binary list
+        return [random.randint(0, 1) for _ in range(self.gene_length)]
+
+    def __repr__(self):
+
+        # Formats output
+        return f"Genetics(gene_number={self.gene_number}, gene_length={self.gene_length}, genes={self.genes})"
+
+@dataclass
 class Subject:
 
-    def __init__(self, number: int):
+    id: int = field(init=False)
+    gene_number: int
+    gene_length: int
+    genetics: Genetics = field(init=False) # Created in post init
 
-        # Id number
-        self.id_number = number
+    last_subject = 0
 
-    # Initializing neural network brain
-    nn_brain = Brain()
+    def __post_init__(self):
+
+        Subject.last_subject += 1
+        self.id = Subject.last_subject
+        self.genetics = Genetics(gene_number=self.gene_number, gene_length=self.gene_length)
 
 
-# RUN
-if __name__ == "__main__":
-    obj = Subject(number=100)
+subject1 = Subject(gene_number=6, gene_length=10)
+print(subject1)
+
+
+
 
